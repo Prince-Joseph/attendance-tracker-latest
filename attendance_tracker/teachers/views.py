bcf8ed883e16cc895aebb1cc6a82728a6932fd3a
cactus001 <namrathashivani144@gmail.com>
2022-10-10 16:17:19 +0530

    modified templates & cleaned code

--- attendance_tracker/teachers/views.py
+++ attendance_tracker/teachers/views.py
@@ -9,16 +9,3 @@ def teachers_dashboard(request):
     # context['timetable']= Timetable.get_daywise_timetable(Timetable,1)
     return render(request,"teachers_dashboard.html",context)
 
-# def URLView(request,student_id):
-    # student_id=user
-    # return redirect('http://localhost:3000/')
-
-# def daywise_timetable_t(request,weekday):
-#     """
-#     displays teachers day wise timetable 
-#     """ 
-    
-#     context={}
-#     weekday=Timetable.objects.get(week_day=weekday)
-#     context['daywise-attendance']=Timetable.get_daywise_timetable(Timetable,weekday)
-#     return render(request,"teachers_dashboard.html",context)
